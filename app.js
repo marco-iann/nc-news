418fd38607fc8f0dc9141b8cbf0c2c8d0d478e16
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-05-16 19:34:29 +0100

    added internal server error handling

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const apiRouter = require('./routes/api');
-const { handleInvalidRoute, handle400, handle404 } = require('./errors');
+const {
+  handleInvalidRoute,
+  handle400,
+  handle404,
+  handle500
+} = require('./errors');
 
 const app = express();
 
@@ -12,5 +17,6 @@ app.use('/*', handleInvalidRoute);
 
 app.use(handle400);
 app.use(handle404);
+app.use(handle500);
 
 module.exports = app;
--- errors.js
+++ errors.js
@@ -25,3 +25,7 @@ exports.handle404 = (err, req, res, next) => {
     res.status(404).send({ msg: err.msg || codes[err.code] });
   else next(err);
 };
+
+exports.handle500 = (err, req, res, next) => {
+  res.status(500).send({ msg: 'internal server error' });
+};
