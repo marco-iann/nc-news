8de7561f389255206479a3403c982e89c74aa35d
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-05-17 11:28:08 +0100

    setup db for production

--- db/index.js
+++ db/index.js
@@ -3,5 +3,10 @@ const testData = require('./test-data/index');
 
 const ENV = process.env.NODE_ENV || 'development';
 
-if (ENV === 'development') module.exports = developmentData;
-else if (ENV === 'test') module.exports = testData;
+const data = {
+  test: testData,
+  development: developmentData,
+  production: developmentData
+};
+
+module.exports = data[ENV];
